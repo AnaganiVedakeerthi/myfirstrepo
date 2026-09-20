3695cba56cbc20da202d2482810efa8b2a8e311b
AnaganiVedakeerthi <92016401+AnaganiVedakeerthi@users.noreply.github.com>
2026-09-20 11:43:58 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+Why Get Certified?
+Any student or professional working in digital fields can benefit from documenting their skills with a certificate.
+
+Certifications help demonstrate knowledge to current or future employers, clients, and collaborators in an increasingly competitive market.
+
+Trusted Learning Platform
+W3Schools has more than two decades of experience teaching coding online.
+
+Our certificates provide a practical and professional way to show your skills in a format that is easy to verify and share.
